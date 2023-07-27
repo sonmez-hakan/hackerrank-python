9c9c370b27bd63662f9f977a4927b6d3d6cc6a22
hakansonmez <hakan@machinarium.co>
2023-07-27 09:52:06 +0300

    standardize-mobile-number-using-decorators

---
+++ ClosuresAndDecorators/standardize-mobile-number-using-decorators.py
@@ -0,0 +1,15 @@
+def wrapper(f):
+    def fun(l):
+        f(['+91 ' + x[-10:-5] + ' ' + x[-5:] for x in l])
+
+    return fun
+
+
+@wrapper
+def sort_phone(l):
+    print(*sorted(l), sep='\n')
+
+
+if __name__ == '__main__':
+    l = [input() for _ in range(int(input()))]
+    sort_phone(l)
